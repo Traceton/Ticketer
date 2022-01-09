58c8a51a3841b5a1e4a31b260b576d09239acc5d
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-08 19:30:44 -0500

    format change for ticket model

--- components/models/Ticket.js
+++ components/models/Ticket.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 
   const ticketSchema = new mongoose.Schema({
 
-  title:{type:String,required:true},description:{type:String,required:true},author:{type:String,required:true},state:{type:String,required:true},createdOn: {
+  title:{type:String,required:true},
+  description:{type:String,required:true},
+  author:{type:String,required:true},
+  state:{type:String,required:true},
+  createdOn: {
     type: Date,
     required: true,
     default: Date.now(),
