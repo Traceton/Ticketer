3eb3a9a44acc636f044bea53ebb9b2b22f44e1cb
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-26 15:22:39 -0500

    small changes

---
+++ components/shared/TicketComment.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TicketComment() {
+  return (
+    <div>
+      <h1>ticket comment itself</h1>
+    </div>
+  );
+}
---
+++ components/shared/TicketCommentList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TicketComments({}) {
+  return (
+    <div>
+      <h1>a comment list will go here</h1>
+    </div>
+  );
+}
