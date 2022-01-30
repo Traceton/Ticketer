ceda1c15ade31ce0df91f00c4a27433c780b2611
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-30 18:05:25 -0500

    updated createTicket.js with new styling

--- pages/tickets/createTicket.js
+++ pages/tickets/createTicket.js
@@ -24,106 +24,109 @@ export default function CreateTicket() {
   };
 
   return (
-    <div className="bg-gray-900 h-screen font-light">
-      <div className="p-2">
-        <div className=" m-4 p-4 rounded-md font-light space-y-8 ">
-          <form onSubmit={createNewTicket}>
-            <div className="flex flex-col justify-center">
-              <h3 className=" m-1 p-1 text-4xl leading-6 font-light text-white">
-                Create New Ticket
-              </h3>
-            </div>
+    <>
+      <div className="bg-gray-900 min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
+            New ticket
+          </h2>
+        </div>
 
-            <div className="text-xl mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-              <div className="sm:col-span-6">
-                <label
-                  htmlFor="title"
-                  className="block text-3xl font-light text-white"
-                >
-                  title
-                </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
-                  <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    autoComplete="title"
-                    className="shadow-sm  border border-transparent block h-10 w-full text-xl bg-gray-600 font-light sm:text-sm border border-gray-300 rounded-md"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-6">
-                <label
-                  htmlFor="description"
-                  className="block text-3xl font-light text-white"
-                >
-                  description
-                </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
-                  <input
-                    type="text"
-                    name="description"
-                    id="description"
-                    autoComplete="description"
-                    className="shadow-sm  border border-transparent block h-10 w-full text-xl font-light sm:text-sm border border-gray-300 rounded-md"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-6">
-                <label
-                  htmlFor="author"
-                  className="block text-3xl font-light text-white"
-                >
-                  author
-                </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
-                  <input
-                    type="text"
-                    name="author"
-                    id="author"
-                    autoComplete="author"
-                    className="shadow-sm  border border-transparent block h-10 w-full text-xl font-light sm:text-sm border border-gray-300 rounded-md"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-6">
+        <div className=" mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-gray-800 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={createNewTicket}>
+              <div>
                 <label
                   htmlFor="state"
-                  className="block text-3xl font-light text-white"
+                  className="block text-sm font-medium text-white"
                 >
-                  state
+                  State
                 </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
+                <div className="mt-1">
                   <input
-                    type="text"
-                    name="state"
                     id="state"
+                    name="state"
+                    type="state"
                     autoComplete="state"
-                    className="shadow-sm  border border-transparent block h-10 w-full text-xl font-light sm:text-sm border border-gray-300 rounded-md"
+                    required
+                    className="appearance-none block w-full px-3 py-2 bg-gray-700 border-0 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white"
                   />
                 </div>
               </div>
-              <div className="m-2 p-2 w-full">
-                <div className="  flex justify-start">
-                  <button
-                    onClick={() => router.back()}
-                    type="button"
-                    className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Back
-                  </button>
-                  <button
-                    type="submit"
-                    className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Save
-                  </button>
+              <div>
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium text-white"
+                >
+                  Title
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="title"
+                    name="title"
+                    type="title"
+                    autoComplete="title"
+                    required
+                    className="appearance-none block w-full px-3 py-2 bg-gray-700 border-0 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white"
+                  />
                 </div>
               </div>
-            </div>
-          </form>
+
+              <div>
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium text-white"
+                >
+                  Description
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="description"
+                    name="description"
+                    type="description"
+                    autoComplete="description"
+                    required
+                    className="appearance-none block w-full px-3 py-2 bg-gray-700 border-0 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="author"
+                  className="block text-sm font-medium text-white"
+                >
+                  Author
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="author"
+                    name="author"
+                    type="author"
+                    autoComplete="author"
+                    required
+                    className="appearance-none block w-full px-3 py-2 bg-gray-700 border-0 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-row ">
+                <button
+                  onClick={() => router.back()}
+                  type="button"
+                  className=" flex justify-center py-2 px-4 mx-1 border border-gray-900 rounded-md shadow-sm text-sm font-medium text-white bg-none hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Back
+                </button>
+                <button
+                  type="submit"
+                  className=" flex grow justify-center py-2 px-4 mx-1 border border-gray-900 rounded-md shadow-sm text-sm font-medium text-white bg-none hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
